15ab04da771f90f281c5f2482f7635fb096065ab
marylandabu <marylandabu@gmail.com>
2024-02-22 16:19:04 -0500

    add stripe

---
+++ src/apis/paymentApi.ts
@@ -0,0 +1,17 @@
+import { AxiosResponse } from "axios";
+import axiosWithAuth from "./axiosWithAuth";
+
+const BASE_URL = 'http://localhost:3000/api/v1/payments'
+export const paymentApi = {
+    createPaymentIntent: async() => {
+        try {
+            const response: AxiosResponse = await axiosWithAuth.post(
+              `${BASE_URL}/create_payment_intent`
+            );
+            
+            return response.data;
+          } catch (error) {
+            console.error(error);
+          }
+    }
+}
